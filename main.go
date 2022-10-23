66c88e6a2d116527213f02d5c5f49a52baec488f
dchf <dchf.jp@gmail.com>
2022-10-23 13:47:39 +0900

    ガチャを行うWebアプリを作成

--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/dchf12/gacha/gacha"
 )
+
+var tmpl = template.Must(template.New("index").Parse(`<!DOCTYPE html>
+<html>
+	<head><title>ガチャ</title></head>
+	<body>
+		<form action="/draw">
+			<label for="num">枚数</input>
+			<input type="number" name="num" min="1" value="1">
+			<input type="submit" value="ガチャを引く">
+		</form>
+		<h1>結果一覧</h1>
+		<ol>{{range .}}
+		<li>{{.}}</li>
+		{{end}}</ol>
+	</body>
+</html>`))
 
 func main() {
 	if err := run(); err != nil {
@@ -20,18 +38,31 @@ func run() error {
 	// ※本当はハンドラ間で競合が起きるのでNG
 	play := gacha.NewPlay(p)
 
-	http.HandleFunc("/draw", func(w http.ResponseWriter, r *http.Request) {
-		if play.Draw() {
-			fmt.Fprintln(w, play.Result())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := tmpl.Execute(w, play.Results()); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
+	})
 
-		if err := play.Err(); err != nil {
-			fmt.Fprintln(w, err.Error(), http.StatusInternalServerError)
-
+	http.HandleFunc("/draw", func(w http.ResponseWriter, r *http.Request) {
+		num, err := strconv.Atoi(r.FormValue("num"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		fmt.Fprintln(w, "残り:", p.DrawableNum())
+		for i := 0; i < num; i++ {
+			if !play.Draw() {
+				break
+			}
+		}
+
+		if err := play.Err(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/", http.StatusFound)
 	})
 
 	return http.ListenAndServe(":8080", nil)
