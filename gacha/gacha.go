04080fe111fe901bbb588c80cdf9dc4dbdd386c0
dchf <dchf.jp@gmail.com>
2022-10-23 13:13:46 +0900

    ガチャAPIを使用

--- gacha/gacha.go
+++ gacha/gacha.go
@@ -1,15 +1,12 @@
 package gacha
 
 import (
-	"math/rand"
-	"time"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-func init() {
-	// 乱数の種を設定する
-	// 現在時刻をUNIX時間にしたものを種とする
-	rand.Seed(time.Now().Unix())
-}
+const baseURL = "https://gohandson-gacha.uc.r.appspot.com/"
 
 type Play struct {
 	player  *Player
@@ -54,24 +51,52 @@ func (p *Play) Draw() bool {
 		return false
 	}
 
-	card := p.draw()
+	card, err := p.draw()
+	if err != nil {
+		p.err = err
+		return false
+	}
 	p.results = append(p.results, card)
 	p.summary[card.Rarity]++
 
 	return p.player.DrawableNum() > 0
 }
 
-func (p *Play) draw() *Card {
-	num := rand.Intn(100)
+func (p *Play) draw() (*Card, error) {
+	q := "スライム:80,オーク:15,ドラゴン:4,イフリート:1"
+	// TODO: GETメソッドのリクエストを生成する
+	// URLはbaseURLの末尾に?q=と変数qの文字列を付加したもの
+	// リクエストボディはnil
+	req, err := http.NewRequest(http.MethodGet, baseURL+"?q="+q, nil)
 
-	switch {
-	case num < 80:
-		return &Card{Rarity: RarityN, Name: "スライム"}
-	case num < 95:
-		return &Card{Rarity: RarityR, Name: "オーク"}
-	case num < 99:
-		return &Card{Rarity: RaritySR, Name: "ドラゴン"}
+	if err != nil {
+		return nil, fmt.Errorf("リクエスト作成:%w", err)
+	}
+
+	// TODO: デフォルトクライアントを使ってリクエストを送る
+	// レスポンスは変数respで受け取る
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		return nil, fmt.Errorf("APIリクエスト:%w", err)
+	}
+	// TODO: ボディをクローズする
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Bodyの読み込み:%w", err)
+	}
+
+	result := string(body)
+	switch result {
+	case "スライム":
+		return &Card{Rarity: RarityN, Name: "スライム"}, nil
+	case "オーク":
+		return &Card{Rarity: RarityR, Name: "オーク"}, nil
+	case "ドラゴン":
+		return &Card{Rarity: RaritySR, Name: "ドラゴン"}, nil
 	default:
-		return &Card{Rarity: RarityXR, Name: "イフリート"}
+		return &Card{Rarity: RarityXR, Name: "イフリート"}, nil
 	}
 }
