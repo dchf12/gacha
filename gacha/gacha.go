5f14358ee81f44226bbdc7f7d1f97bd0086730b1
dchf <dchf.jp@gmail.com>
2022-10-23 13:24:58 +0900

    HTTPサーバーを作成してガチャの結果を表示

--- gacha/gacha.go
+++ gacha/gacha.go
@@ -64,23 +64,14 @@ func (p *Play) Draw() bool {
 
 func (p *Play) draw() (*Card, error) {
 	q := "スライム:80,オーク:15,ドラゴン:4,イフリート:1"
-	// TODO: GETメソッドのリクエストを生成する
-	// URLはbaseURLの末尾に?q=と変数qの文字列を付加したもの
-	// リクエストボディはnil
 	req, err := http.NewRequest(http.MethodGet, baseURL+"?q="+q, nil)
-
 	if err != nil {
 		return nil, fmt.Errorf("リクエスト作成:%w", err)
 	}
-
-	// TODO: デフォルトクライアントを使ってリクエストを送る
-	// レスポンスは変数respで受け取る
 	resp, err := http.DefaultClient.Do(req)
-
 	if err != nil {
 		return nil, fmt.Errorf("APIリクエスト:%w", err)
 	}
-	// TODO: ボディをクローズする
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
